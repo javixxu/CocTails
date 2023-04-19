ecb9bb8e98f49074139a50099b9f73d91d3840bd
Javier <javcomas@ucm.es>
2023-04-19 20:02:43 +0200

    config de daikiri , faltan un par de cosas me da pereza estoy vagete

--- Assets/Scripts/TipoObjecto.cs
+++ Assets/Scripts/TipoObjecto.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum TypeObecto { Naranja, Cereza, Limon, Hielo, Azucar };
+public enum TypeObecto { Naranja, Cereza, Limon, Hielo, Azucar, Sombrilla };
 public class TipoObjecto : MonoBehaviour{   
     public TypeObecto myType;
 }
