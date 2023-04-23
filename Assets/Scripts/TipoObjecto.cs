2821748f34db169fa59afc63b4bcd9768ff815b2
Javier <javcomas@ucm.es>
2023-04-23 19:30:30 +0200

    cositas

--- Assets/Scripts/TipoObjecto.cs
+++ Assets/Scripts/TipoObjecto.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum TypeObecto { Naranja, Cereza, Limon, Hielo, Azucar, Sombrilla };
+public enum TypeObecto { Naranja, Cereza, Limon, Hielo, Azucar, Sombrilla, Bengala };
 public class TipoObjecto : MonoBehaviour{   
     public TypeObecto myType;
 }
