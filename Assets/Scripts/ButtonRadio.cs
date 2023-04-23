0cad2ed5637197333646c34af49350dc40760446
sarais02 <sarais02@ucm.es>
2023-04-23 23:02:42 +0200

    Radio

---
+++ Assets/Scripts/ButtonRadio.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+using Valve.VR;
+using static Valve.VR.InteractionSystem.Hand;
+
+public class ButtonRadio : MonoBehaviour
+{
+    public bool power;
+    Radio radio;
+    bool isHandOver;
+    void Start()
+    {
+        radio = GetComponentInParent<Radio>();
+        isHandOver=false;
+    }
+    protected virtual void OnHandHoverBegin(Hand hand)
+    {
+        Debug.Log("BOTON PULSADO");
+        int i = radio.index + 1;
+        if (i > 3) i = 0;
+        isHandOver = true;
+        if (power)
+        {
+            radio.audioSource.Stop();
+        }
+        else
+        {
+            radio.index = i;
+            radio.audioSource.clip = radio.songs[i];
+            radio.audioSource.Play();
+        }
+    }
+
+
+    //-------------------------------------------------
+    protected virtual void OnHandHoverEnd(Hand hand)
+    {
+        isHandOver = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (/*other.gameObject.GetComponent<Hand>()!=null*/other.CompareTag("Hand"))
+        {
+            Debug.Log("BOTON PULSADO");
+            if (power)
+            {
+                radio.audioSource.Stop();
+            }
+            else
+            {
+                int i = (radio.index + 1) % 4;
+                radio.index = i;
+                radio.audioSource.clip = radio.songs[i];
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Radio.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radio : MonoBehaviour
+{
+    //public float angle;
+    public AudioSource audioSource;
+    public AudioClip[] songs;
+    public GameObject powerButton;
+    public GameObject changeSongButton;
+    public int index;
+
+    Vector3 previousPosition;
+
+    void Start()
+    {
+        index = 0;
+    }
+
+    void Update()
+    {
+        // Calcula la rotación actual del dial
+        //Vector3 direction = transform.position - previousPosition;
+        //float currentAngle = Vector3.SignedAngle(direction, Vector3.forward, Vector3.up);
+        //angle += currentAngle;
+        //previousPosition = transform.position;
+
+        //// Asegúrate de que el ángulo no sea negativo
+        //if (angle < 0)
+        //{
+        //    angle += 360;
+        //}
+
+        //// Calcula el índice de la canción actual
+        //int songIndex = Mathf.FloorToInt(angle / 360f * songs.Length);
+
+        //// Si la canción actual es diferente de la anterior, cámbiala y reprodúcela
+        //if (audioSource.clip != songs[songIndex])
+        //{
+        //    audioSource.Stop();
+        //    audioSource.clip = songs[songIndex];
+        //    audioSource.Play();
+        //}
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        // Si el usuario está agarrando el dial, actualiza su posición
+        //if (other.CompareTag("Hand"))
+        //{
+        //    //previousPosition = other.transform.position;
+        //}
+    }
+}
