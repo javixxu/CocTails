e1a0a92ce3799691e50ce590333f8be2f5f861c7
sarais02 <sarais02@ucm.es>
2023-04-24 23:47:36 +0200

    Animacion botones radio

--- Assets/Scripts/ButtonRadio.cs
+++ Assets/Scripts/ButtonRadio.cs
@@ -9,6 +9,7 @@ using static Valve.VR.InteractionSystem.Hand;
 public class ButtonRadio : MonoBehaviour
 {
     public bool power;
+    public Animator animator;
     Radio radio;
     bool isHandOver;
     void Start()
@@ -19,6 +20,7 @@ public class ButtonRadio : MonoBehaviour
     protected virtual void OnHandHoverBegin(Hand hand)
     {
         Debug.Log("BOTON PULSADO");
+        animator.SetBool("Pulsado", true);
         int i = radio.index + 1;
         if (i > 3) i = 0;
         isHandOver = true;
@@ -39,6 +41,7 @@ public class ButtonRadio : MonoBehaviour
     protected virtual void OnHandHoverEnd(Hand hand)
     {
         isHandOver = false;
+        animator.SetBool("Pulsado", false);
     }
     //private void OnTriggerEnter(Collider other)
     //{
